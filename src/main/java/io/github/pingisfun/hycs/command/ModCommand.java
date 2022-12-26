a17ea469aeb3c1d9af683ba1184017ab78102245
PingIsFun <pingisfun@protonmail.com>
2022-12-26 10:28:39 +0100

    Command stuff idk it's bad

--- src/main/java/io/github/pingisfun/hycs/command/ModCommand.java
+++ src/main/java/io/github/pingisfun/hycs/command/ModCommand.java
@@ -1,18 +1,18 @@
 package io.github.pingisfun.hycs.command;
 
-import cc.polyfrost.oneconfig.utils.commands.annotations.*;
+import cc.polyfrost.oneconfig.utils.commands.annotations.Command;
+import cc.polyfrost.oneconfig.utils.commands.annotations.Description;
+import cc.polyfrost.oneconfig.utils.commands.annotations.Main;
+import cc.polyfrost.oneconfig.utils.commands.annotations.SubCommandGroup;
 import io.github.pingisfun.hycs.HyChatStatsMod;
 import io.github.pingisfun.hycs.util.ChatUtil;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 @Command(value = HyChatStatsMod.MODID, description = "Access the " + HyChatStatsMod.NAME + " GUI.")
 public class ModCommand {
     @Main
     private void main() {
-        HyChatStatsMod.INSTANCE.config.openGui();
+        HyChatStatsMod.config.openGui();
     }
 
     @SubCommandGroup(value = "bedwars", aliases = {"bw"})
--- src/main/java/io/github/pingisfun/hycs/command/TestCommand.java
+++ src/main/java/io/github/pingisfun/hycs/command/TestCommand.java
@@ -1,15 +1,8 @@
 package io.github.pingisfun.hycs.command;
 
-import io.github.pingisfun.hycs.HyChatStatsMod;
 import cc.polyfrost.oneconfig.utils.commands.annotations.Command;
-import cc.polyfrost.oneconfig.utils.commands.annotations.Main;
-import io.github.pingisfun.hycs.util.ChatUtil;
+import io.github.pingisfun.hycs.HyChatStatsMod;
 
 @Command(value = "test_" + HyChatStatsMod.MODID, description = "Access the " + HyChatStatsMod.NAME + " GUI.")
 public class TestCommand {
-
-    @Main
-    private static void main() {
-        ChatUtil.test();
-    }
 }
