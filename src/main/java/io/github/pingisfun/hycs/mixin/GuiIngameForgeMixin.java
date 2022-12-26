9c82b013ecefa314764a3441b443e278bbf7c819
PingIsFun <pingisfun@protonmail.com>
2022-12-26 12:07:37 +0100

    Bufgix

--- src/main/java/io/github/pingisfun/hycs/mixin/GuiIngameForgeMixin.java
+++ src/main/java/io/github/pingisfun/hycs/mixin/GuiIngameForgeMixin.java
@@ -1,7 +1,12 @@
 package io.github.pingisfun.hycs.mixin;
 
+import cc.polyfrost.oneconfig.utils.hypixel.HypixelUtils;
+import io.github.pingisfun.hycs.HyChatStatsMod;
+import io.github.pingisfun.hycs.config.ModConfig;
 import io.github.pingisfun.hycs.events.TitleEvent;
+import io.github.pingisfun.hycs.util.ChatUtil;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiIngame;
 import net.minecraftforge.client.GuiIngameForge;
 import net.minecraftforge.common.MinecraftForge;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,17 +18,20 @@ import java.util.Arrays;
 import java.util.List;
 
 @Mixin(value = GuiIngameForge.class, remap = false)
-public class GuiIngameForgeMixin {
+public class GuiIngameForgeMixin extends GuiIngame {
     private static final String[] IGNORED_TITLES = {"§r", ""};
+
+    public GuiIngameForgeMixin(Minecraft minecraft) {
+        super(minecraft);
+    }
 
     @Inject(method = "renderTitle", at = @At("HEAD"))
     private void onRenderTitle(int l, int age, float opacity, CallbackInfo ci) {
-//        if (!HypixelUtils.INSTANCE.isHypixel()) {
-//            return;
-//        }
-        Minecraft client = Minecraft.getMinecraft();
-        String title = client.ingameGUI.displayedTitle;
-        String subtitle = client.ingameGUI.displayedSubTitle;
+        if (!HypixelUtils.INSTANCE.isHypixel() || !HyChatStatsMod.config.enabled) {
+            return;
+        }
+        String title = this.displayedTitle;
+        String subtitle = this.displayedSubTitle;
 
         List<String> ignoredTitlesList = Arrays.asList(IGNORED_TITLES);
         if (ignoredTitlesList.contains(title) && ignoredTitlesList.contains(subtitle)) {
