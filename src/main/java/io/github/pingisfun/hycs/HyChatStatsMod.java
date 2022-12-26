4cd8b0fef133280efa1802d118643d2c67a714dc
PingIsFun <pingisfun@protonmail.com>
2022-12-26 10:28:10 +0100

    refactor

--- src/main/java/io/github/pingisfun/hycs/HyChatStatsMod.java
+++ src/main/java/io/github/pingisfun/hycs/HyChatStatsMod.java
@@ -1,22 +1,28 @@
 package io.github.pingisfun.hycs;
 
+import io.github.pingisfun.hycs.auto.BWGameStats;
 import io.github.pingisfun.hycs.command.ModCommand;
 import io.github.pingisfun.hycs.command.TestCommand;
 import io.github.pingisfun.hycs.config.ModConfig;
 import cc.polyfrost.oneconfig.utils.commands.CommandManager;
-
-@net.minecraftforge.fml.common.Mod(modid = HyChatStatsMod.MODID, name = HyChatStatsMod.NAME, version = HyChatStatsMod.VERSION)
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.EventBus;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+@Mod(modid = HyChatStatsMod.MODID, name = HyChatStatsMod.NAME, version = HyChatStatsMod.VERSION)
 public class HyChatStatsMod {
     public static final String MODID = "@ID@";
     public static final String NAME = "@NAME@";
     public static final String VERSION = "@VER@";
-    @net.minecraftforge.fml.common.Mod.Instance(MODID)
+    @Mod.Instance(MODID)
     public static HyChatStatsMod INSTANCE;
-    public ModConfig config;
+    public static ModConfig config;
 
-    @net.minecraftforge.fml.common.Mod.EventHandler
-    public void onFMLInitialization(net.minecraftforge.fml.common.event.FMLInitializationEvent event) {
+    @Mod.EventHandler
+    public void onFMLInitialization(FMLInitializationEvent event) {
         config = new ModConfig();
         CommandManager.INSTANCE.registerCommand(new ModCommand());
+//        CommandManager.INSTANCE.registerCommand(new TestCommand());
+        MinecraftForge.EVENT_BUS.register(new BWGameStats());
     }
 }
