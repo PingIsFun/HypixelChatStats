462d6b3e939ac21f5f23da7ba9e4b124e71f6b2b
PingIsFun <pingisfun@protonmail.com>
2022-12-26 10:26:14 +0100

    Add API key and BWGameStats toggle to confitg

--- src/main/java/io/github/pingisfun/hycs/config/ModConfig.java
+++ src/main/java/io/github/pingisfun/hycs/config/ModConfig.java
@@ -1,30 +1,28 @@
 package io.github.pingisfun.hycs.config;
 
 import io.github.pingisfun.hycs.HyChatStatsMod;
-import io.github.pingisfun.hycs.hud.TestHud;
 import cc.polyfrost.oneconfig.config.Config;
-import cc.polyfrost.oneconfig.config.annotations.HUD;
 import cc.polyfrost.oneconfig.config.annotations.Switch;
+import cc.polyfrost.oneconfig.config.annotations.Text;
 import cc.polyfrost.oneconfig.config.data.Mod;
 import cc.polyfrost.oneconfig.config.data.ModType;
 
 public class ModConfig extends Config {
 
-    @HUD(
-            name = "Very cool HUD",
-            subcategory = "Test"
+    @Text(
+            name = "Hypixel API Key",
+            secure = true, multiline = false
     )
-    public static TestHud testHud = new TestHud();
+    public static String hypixelApiKey = "";
 
+    // Auto
     @Switch(
-            name = "Test",
-            subcategory = "Test",
-            size = 2
+            name = "Bedwars Game Stats",
+            category = "Auto"
     )
-    public static boolean test = true;
-
+    public static boolean isBedwarsGameStatsEnabled = false;
     public ModConfig() {
-        super(new Mod(HyChatStatsMod.NAME, ModType.UTIL_QOL), HyChatStatsMod.MODID + ".json");
+        super(new Mod(HyChatStatsMod.NAME, ModType.HYPIXEL), HyChatStatsMod.MODID + ".json");
         initialize();
     }
 }
